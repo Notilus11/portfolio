e73e7196a4bde15318d86288aae627cc9b027c97
Yann Malaret <yann.malaret@epitech.eu>
2022-12-01 15:10:30 +0100

    test_n_page

---
+++ pages/repo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mainBox">
+        test
+    </div>
+</template>
+
+<style scoped>
+
+.mainBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+a {
+    text-decoration: underline;
+}
+</style>
